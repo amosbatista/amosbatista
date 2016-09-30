e8b39d57372b79c433dc2d8c9615350889430bf0
Amós Batista <amos.silva@gmail.com>
2016-09-30 18:47:30 -0300

    Creation of gulpfile.js, and the first task of the gulp automathization

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+var pug	= require('gulp-pug');
+var less = require ('gulp-less');
+
+gulp.task ('tarefa-pug', function(){
+	return gulp.src("views/*.pug").pipe(pug()).pipe( gulp.dest('dist/'));
+});
