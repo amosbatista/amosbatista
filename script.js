f70a7fdda20b9ffbaa2e94b0e0e8d27b3a34deef
Amós Batista <amos.silva@gmail.com>
2016-12-24 22:31:42 -0200

    Links animation and transitions.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Comments char list, for the home
+// Comments char animation, for the home
 var commentComponent = document.getElementById("codeCommentContainer");
 var commentCharList = [
 	{
@@ -17,53 +17,188 @@ commentComponent.className = "show-comment-animated code-comment " + commentChar
 
 
 
+
+
+
+
+
 /* Arrows animations */
-var aboutLink = document.getElementById("linkAbout");
-var transitionID = undefined;
-var transitionValue = 0;
-var accellFactor = 1;
+/* Set a fade-in effect to another links who not hover
+*/
+var _fadeOtherLinks = function (linkList){
 
-aboutLink.addEventListener("mouseenter", function (mouseEvent){
-	
-	var icon = mouseEvent.currentTarget.children[0];
+	return function(hoveredLink){
 
-	transitionID = setInterval(function(){
+		linkList.forEach(function(link){
 
-		if(accellFactor <= 50)
-			accellFactor = accellFactor + 2;
-		transitionValue = transitionValue - accellFactor;
+			if(link != hoveredLink){
 
-		// Return the arrow to above the link
-		if(transitionValue < -250)
-			transitionValue = 250;
+				if(hoveredLink == undefined)
+					link.classList.remove("home-fade-to-white");
+				else
+					link.classList.add("home-fade-to-white");
+			}
 
-		icon.style.transform = "translate(0px, " + transitionValue + "% )";
-	}, 20);
-});
+			else
+				link.classList.remove("home-fade-to-white");
+		});
+	}	
+}
 
-aboutLink.addEventListener("mouseleave", function (mouseEvent){
-	var icon = mouseEvent.currentTarget.children[0];
+/* Receives a function, set the obj translation. 
+This must receives the transition value, and must return a {X, Y} object*/
+var mainLinkAnimation = function(){
 
-	clearInterval(transitionID);
+	var lastElement = undefined;
+	var animationLimit = 320;
+	var transitionValue = 0;
+	var _transitionComponent = undefined;
+	var finishTransitionID = undefined;
+	var animationID = undefined;
 
-	finishTransitionID = setInterval(function(){
+	return {
 
-		// Range to end the transition
-		if(transitionValue > -10 && transitionValue < 10){
-			icon.style.transform = "translate(0px, 0%)";
-			clearInterval(finishTransitionID);
-			finishTransitionID = undefined;
-			accellFactor = 1;
-			return;
+		start: function (spanElement, transitionComponent){
+
+			// Check an occourring animation.
+			if( animationID != undefined || finishTransitionID != undefined )
+				return;
+
+			lastElement = spanElement;
+			_transitionComponent = transitionComponent;
+
+			// Start of process
+			var accellFactor = 1;
+
+			animationID = setInterval(function(){
+
+				if(accellFactor <= 50)
+					accellFactor = accellFactor + 0.3;
+				transitionValue = transitionValue - accellFactor;
+
+				// Return the arrow to above the link
+				if(transitionValue < -animationLimit)
+					transitionValue = animationLimit;
+
+				var transitionObj = _transitionComponent(transitionValue);
+
+				spanElement.style.transform = "translate( " + transitionObj.X + "%, " + transitionObj.Y + "% )";
+				
+				
+			}, 15);
+		},
+
+		// At same time the process is rolling, return a function, that allows to stop the process
+		stop: function(){
+
+			// Check an occourring animation.
+			if( finishTransitionID != undefined || lastElement == undefined )
+				return;
+
+			clearInterval(animationID);
+			animationID = undefined;
+
+			transitionValue = animationLimit;
+
+			finishTransitionID = setInterval(function(){
+
+				transitionValue = transitionValue - 10;
+				transitionObj = _transitionComponent(transitionValue);
+
+				// Range to end the transition
+				if(transitionValue > -10 && transitionValue < 10){
+					lastElement.style.transform = "translate(0%, 0%)";
+					clearInterval(finishTransitionID);
+
+					finishTransitionID = undefined;
+					lastElement = undefined;
+					return;
+				}
+
+				lastElement.style.transform = "translate(" + transitionObj.X + "%, " + transitionObj.Y + "%)";
+
+			}, 12);
 		}
 
-		if(transitionValue < 0)
-			transitionValue = transitionValue + 5;
-		else
-			transitionValue = transitionValue - 5;
+	}
+}
 
-		icon.style.transform = "translate(0px, " + transitionValue + "%)";
+var linksDOMObjects = [
+	document.getElementById("linkAbout"),
+	document.getElementById("linkGallery"),
+	document.getElementById("linkBlog"),
+	document.getElementById("linkPortfolio")
+];
 
-	}, 5);
+var animationObj = mainLinkAnimation();
+var fadeOtherLinks = _fadeOtherLinks(linksDOMObjects);
 
+
+
+
+
+linksDOMObjects[0].addEventListener("mouseenter", function (mouseEvent){
+
+	animationObj.start(mouseEvent.currentTarget.children[0], function(transitionValue){
+
+		return {
+			X: 0, Y: transitionValue
+		}
+
+	});
+	fadeOtherLinks(linksDOMObjects[0]);
+});
+linksDOMObjects[0].addEventListener("mouseleave", function (mouseEvent){
+	animationObj.stop();
+	fadeOtherLinks();
+});
+
+
+linksDOMObjects[1].addEventListener("mouseenter", function (mouseEvent){
+	/* The children array changes, because the position in HTML code is different*/
+	animationObj.start(mouseEvent.currentTarget.children[1], function(transitionValue){
+
+		return {
+			X: -transitionValue, Y: 0
+		}
+
+	});
+	fadeOtherLinks(linksDOMObjects[1]);
+});
+linksDOMObjects[1].addEventListener("mouseleave", function (mouseEvent){
+	animationObj.stop();
+	fadeOtherLinks();
+});
+
+
+linksDOMObjects[2].addEventListener("mouseenter", function (mouseEvent){
+	/* The children array changes, because the position in HTML code is different*/
+	animationObj.start(mouseEvent.currentTarget.children[1], function(transitionValue){
+
+		return {
+			X: 0, Y: -transitionValue
+		}
+
+	});
+	fadeOtherLinks(linksDOMObjects[2]);
+});
+linksDOMObjects[2].addEventListener("mouseleave", function (mouseEvent){
+	animationObj.stop();
+	fadeOtherLinks();
+});
+
+linksDOMObjects[3].addEventListener("mouseenter", function (mouseEvent){
+	/* The children array changes, because the position in HTML code is different*/
+	animationObj.start(mouseEvent.currentTarget.children[0], function(transitionValue){
+
+		return {
+			X: transitionValue, Y: 0
+		}
+
+	});
+	fadeOtherLinks(linksDOMObjects[3]);
+});
+linksDOMObjects[3].addEventListener("mouseleave", function (mouseEvent){
+	animationObj.stop();
+	fadeOtherLinks();
 });
