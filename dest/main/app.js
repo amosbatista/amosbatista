1b37c42d482fcc8a5af1b8c4df19861344be3053
Amós Batista <amos.silva@gmail.com>
2017-01-22 15:32:50 -0200

    Easy scroll activity

--- dest/main/app.js
+++ dest/main/app.js
@@ -34229,58 +34229,66 @@ appWebSite.controller('aboutCtrl', [
 		}
 	}
 ])
-// The scroll down click. When clicked, emit the 'startEasyScroll' event
-appWebSite.directive('easyScroll', [ function(){
+// The directive that set the footer link scroll animation
+appWebSite.directive('easyScrollLink', [ function(){
 
-	var showElement = true;
-	var linkOpacity = 1;
-	var animation;
+	// The function process to hide the link
+	var scrollAndHideProcessFunction = function(scope, element){
+		var showElement = true;
+		var linkOpacity = 1;
+		var animation;
+
+		// Hide the link
+		animation = setInterval(function (){
+
+			// Animation end
+			if(linkOpacity <= 0 ){
+				linkOpacity = 0;
+				element[0].style.display = 'none';
+				clearInterval (animation);
+			}
+
+			// Setup transition
+			else{
+				linkOpacity = (linkOpacity - 0.2).toFixed(2);
+			}
+
+			element[0].children[0].style.opacity = linkOpacity;
+
+		}, 25);
+		
+	}
 
 	return {
 		restrict: "A",
 		link: function(scope, element){
+
+			// Activate the transition into this directive when receive the event
+			scope.$on ("startEasyScroll", function(){
+				scrollAndHideProcessFunction(scope, element);
+			});
+
+			// Emit the event at click, to activate to another directives
 			element.bind("click", function(clickEvent){
-				clickEvent.preventDefault();
-
-				// Hide the link
-				animation = setInterval(function (){
-
-					// Animation end
-					if(linkOpacity <= 0 ){
-						linkOpacity = 0;
-						element[0].style.display = 'none';
-						clearInterval (animation);
-					}
-
-					// Setup transition
-					else{
-						linkOpacity = (linkOpacity - 0.2).toFixed(2);
-					}
-
-					element[0].children[0].style.opacity = linkOpacity;
-
-				}, 25);
-
-
 				scope.$emit ('startEasyScroll');
 			});
+			
 		}
-	};
-
+	}
 }]);
 
 
 
-// The page header directive. When receives the 'startEasyScroll', scroll the element
+// The page header directive. When receives the 'startEasyScroll', scroll, to hide the element
 appWebSite.directive('easyScrollTop', function(){
-
-	var translateY = 1;
-	var translateOpac = 0.1;
-	var animation; 
 
 	return {
 		restrict: "A",
 		link: function(scope, element){
+
+			var translateY = 1;
+			var translateOpac = 0.1;
+			var animation; 
 
 			scope.$on ("startEasyScroll", function(){
 
@@ -34290,6 +34298,7 @@ appWebSite.directive('easyScrollTop', function(){
 					if(translateY >= window.innerHeight){
 						translateY = window.innerHeight;
 						clearInterval (animation);
+						animation = undefined;
 					}
 
 					// Process the values
@@ -34302,6 +34311,76 @@ appWebSite.directive('easyScrollTop', function(){
 					element[0].style.opacity = translateOpac;
 				}, 50);
 			});
+		}
+	}
+});
+
+
+// The page content directive. When receives the 'startEasyScroll', scroll and show the element
+appWebSite.directive('easyScrollContent', function(){
+
+	return {
+		restrict: "A",
+		link: function(scope, element){
+
+			var animation; 
+			var translateOpac = 0.1;
+			var translateY = 175;
+
+			scope.$on ("startEasyScroll", function(){
+
+				animation = setInterval(function (){
+
+					// Animation end
+					if( translateY <= -75 ){
+						translateOpac = 1;
+						/*translateY = 0;*/
+						clearInterval(animation);
+						animation = undefined;
+					}
+
+					// Animation process
+					else{
+						translateOpac = translateOpac + 0.4;
+						translateY = translateY - 50;
+					}
+
+					// Transition set
+					element[0].style.transform = 'translateY(' + translateY + 'px)';
+					element[0].style.opacity = translateOpac;
+
+				}, 50);
+			});
+		}
+	}
+});
+
+
+// The directive that hold a trigger to activate the easy scroll
+appWebSite.directive('easyScroll', function(){
+
+	
+
+	return {
+		restrict: "A",
+		link: function(scope, element){
+
+			var easyScrollEvent = function(scrollEvent){
+				console.log("I'm scrolling");
+
+				if( document.body.scrollTop > 0 ){
+					scope.$emit ('startEasyScroll');
+				}
+			}
+
+			// When load the page, force the scroll to the top
+			/*document.removeEventListener("scroll", easyScrollEvent);*/
+			document.scrollTop = 0;
+
+			document.addEventListener("scroll", easyScrollEvent, true);
+
+			
+
 		}
 	}
 });
