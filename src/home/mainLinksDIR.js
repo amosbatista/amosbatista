c76dc020f07fec45b1be356e1a281fbf9a279603
Amós Batista <amos.silva@gmail.com>
2017-03-19 15:55:51 -0300

    Css animation at home

--- src/home/mainLinksDIR.js
+++ src/home/mainLinksDIR.js
@@ -13,11 +13,7 @@ angular.module('site.home').directive('mainLink', ['$timeout', function(timeout)
 
 		
 		link: function (scope, element){
-			var animationLimit = 320;
-			var transitionValue = 0;
-			var animationID = undefined;
-			var linkElement;
-
+			
 			var linkList = [];
 
 			/* All 4 link types */
@@ -25,41 +21,22 @@ angular.module('site.home').directive('mainLink', ['$timeout', function(timeout)
 				about : {
 					id: "linkAbout",
 					iconName: "fa-arrow-up",
-					transition: function(transitionValue){
-						return {
-							X: 0, Y: transitionValue
-						}
-					}
 				},
 
 				gallery : {
 					id: "linkGallery",
 					iconName: "fa-arrow-right",
-					transition: function(transitionValue){
-						return {
-							X: -transitionValue, Y: 0
-						}
-					}
 				},
 
 				blog : {
 					id: "linkBlog",
 					iconName: "fa-arrow-down",
-					transition: function(transitionValue){
-						return {
-							X: 0, Y: -transitionValue
-						}
-					}
+					
 				},
 
 				portfolio : {
 					id: "linkPortfolio",
 					iconName: "fa-arrow-left",
-					transition: function(transitionValue){
-						return {
-							X: transitionValue, Y: 0
-						}
-					}
 				},
 			};
 
@@ -67,7 +44,7 @@ angular.module('site.home').directive('mainLink', ['$timeout', function(timeout)
 			// Select the definition set by the name, in the HTML page
 			scope.linkId = linkDefinition[scope.linkName].id;
 			scope.faIconName = linkDefinition[scope.linkName].iconName;
-			var _transitionComponent = linkDefinition[scope.linkName].transition;
+			
 
 			// Get the all link, to the fade effect		
 			timeout(function () {
@@ -77,66 +54,11 @@ angular.module('site.home').directive('mainLink', ['$timeout', function(timeout)
 				linkList.push(document.getElementById(linkDefinition.portfolio.id));		
 			});
 
-			/* Animation Process*/
-			element.on('mouseenter', function (event){
-
-				// Start of process
-				var accellFactor = 1;
-				transitionValue = 0;
-
-				linkElement = document.getElementById (scope.linkId);
-
-				
-
-				animationID = setInterval(function(){
-
-					if(accellFactor <= 50)
-						accellFactor = accellFactor + 0.3;
-					transitionValue = transitionValue - accellFactor;
-
-					// Return the arrow to above the link
-					if(transitionValue < -animationLimit)
-						transitionValue = animationLimit;
-
-					var transitionObj = _transitionComponent(transitionValue);
-
-					// Translate the icon elements (there's 2, because gallery has another position)
-					linkElement.children[0].style.transform = "translate( " + transitionObj.X + "%, " + transitionObj.Y + "% )";
-					linkElement.children[2].style.transform = "translate( " + transitionObj.X + "%, " + transitionObj.Y + "% )";
-					
-				}, 15);	
-
-			});
-
-			element.on('mouseleave', function (event){
-
-				clearInterval(animationID);
-				animationID = undefined;
-
-				transitionValue = animationLimit;
-
-				finishTransitionID = setInterval(function(){
-
-					transitionValue = transitionValue - 10;
-					transitionObj = _transitionComponent(transitionValue);
-
-					// Range to end the transition
-					if(transitionValue > -10 && transitionValue < 10){
-						linkElement.children[0].style.transform = "translate(0%, 0%)";
-						clearInterval(finishTransitionID);
-
-						finishTransitionID = undefined;
-						return;
-					}
-
-					linkElement.children[0].style.transform = "translate(" + transitionObj.X + "%, " + transitionObj.Y + "%)";
-					linkElement.children[2].style.transform = "translate(" + transitionObj.X + "%, " + transitionObj.Y + "%)";
-
-				}, 12);
-			});
-
 			/* Fade links process */
 			element.on('mouseenter', function (event){
+
+				// Swing effect
+				element[0].classList.add("link-swing");
 
 				linkList.forEach(function(link){
 
@@ -147,6 +69,8 @@ angular.module('site.home').directive('mainLink', ['$timeout', function(timeout)
 			});
 
 			element.on('mouseleave', function (event){
+
+				element[0].classList.remove("link-swing");
 
 				linkList.forEach(function(link){
 					link.classList.remove("home-fade-to-white");
@@ -176,10 +100,7 @@ angular.module('site.home').directive('mainLink', ['$timeout', function(timeout)
 					case 'gallery': 
 						element.parent()[0].classList.add('slide-gallery');
 						break;
-				}
-				
-				
-				
+				}				
 			}
 
 			scope.$on("$destroy", function(event){
