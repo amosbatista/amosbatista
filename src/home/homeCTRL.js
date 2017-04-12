9d94e29254dee160e5e46ae3f0b0afdeb9e1b54e
Amós Batista <amos.silva@gmail.com>
2017-04-11 23:11:52 -0300

    Metadata set

--- src/home/homeCTRL.js
+++ src/home/homeCTRL.js
@@ -1,11 +1,22 @@
 angular.module('site.home').controller('homeCtrl', [
 	'$scope',
 	'$state',
+	"$rootScope",
 	function(
 		scope,
-		state
+		state,
+		root
 	){
 		scope.$emit('toHideLoadScreen');
+
+		root.ogMetadata = {
+			url: window.location.href,
+			title: "AmosBatista.com",
+			description: "Bem-vindos ao meu site, blog, portfólio e galeria.",
+			image: window.location.origin + "/img/print-home.jpg"
+		}
+
+
 
 		scope.goToAboutPage = function(){
 			scope.$emit('toShowLoadScreen');
