5585c031f96aef4f48538f68bab6716cdaa523a3
Amós Batista <amos.silva@gmail.com>
2017-04-12 12:59:45 -0300

    Sharing is working

--- dest/main/app.js
+++ dest/main/app.js
@@ -56136,106 +56136,6 @@ angular.module('common.easyScroll').directive('easyScrollContent', function(){
 		}
 	}
 });
-angular.module('common.header', []);
-
-angular.module('common.header').directive("myHeader", [
-	'$timeout',
-	'$state',
-	function(
-		timeout,
-		state
-	){
-		return {
-			restrict: "A",
-			templateUrl: "_header.html",
-			replace: true,
-			link: function (scope, element){
-
-				/* Links redirects*/
-				var headerLinkToHome = function(){
-					scope.$emit('toShowLoadScreen');
-					state.go('home')
-				}
-
-				var headerLinkToGallery = function(){
-					scope.$emit('toShowLoadScreen');
-					state.go('gallery')
-				}
-
-				var headerLinkToAbout = function(){
-					scope.$emit('toShowLoadScreen');
-					state.go('about')
-				}
-
-				var headerLinkToBlog = function(){
-					scope.$emit('toShowLoadScreen');
-					state.go('blog')
-				}
-
-				var headerLinkToPortfolio = function(){
-					scope.$emit('toShowLoadScreen');
-					state.go('portfolio')
-				}
-
-				var headerLinkList = [
-					{
-						name: 'Home',
-						location: 'home',
-						iconClass: 'fa-home',
-						linkFunction: headerLinkToHome,
-						linkClass: 'link-home'
-					},
-					{
-						name: 'About',
-						location: 'about',
-						iconClass: 'fa-arrow-up',
-						linkFunction: headerLinkToAbout,
-						linkClass: 'link-about'
-					},
-					{
-						name: 'Gallery',
-						location: 'gallery',
-						iconClass: 'fa-arrow-right',
-						linkFunction: headerLinkToGallery,
-						linkClass: 'link-gallery'
-					},
-					{
-						name: 'Portfolio',
-						location: 'portfolios',
-						iconClass: 'fa-arrow-left',
-						linkFunction: headerLinkToPortfolio,
-						linkClass: 'link-portfolio'
-					},
-					{
-						name: 'Blog',
-						location: 'blog',
-						iconClass: 'fa-arrow-down',
-						linkFunction: headerLinkToBlog,
-						linkClass: 'link-blog'
-					},
-					
-				];
-
-				// Set all links to show (remove the link of current page)
-				scope.headerLinkToShow = headerLinkList.filter( function (link ){
-					return link.location != state.current.name;
-				});
-
-
-				/* Header behavior with the footer. Header hides, when footers shows and vice-versa */
-				/* Event to detect the apperance of footer */
-				scope.$on("footerIsRising", function(){
-					element[0].classList.add('hide-header');
-					element[0].classList.remove('show-header');
-				});
-				scope.$on("footerIsHiding", function(){
-					element[0].classList.add('show-header');
-					element[0].classList.remove('hide-header');
-				});
-			}
-		}
-	}
-]);
 /* The footer directive*/
 angular.module('common.footer', []);
 
@@ -56342,6 +56242,106 @@ angular.module('common.footer').directive("myFooter", [
 						element[0].classList.add('hide-footer');
 						element[0].classList.remove('show-footer');
 					}
+				});
+			}
+		}
+	}
+]);
+angular.module('common.header', []);
+
+angular.module('common.header').directive("myHeader", [
+	'$timeout',
+	'$state',
+	function(
+		timeout,
+		state
+	){
+		return {
+			restrict: "A",
+			templateUrl: "_header.html",
+			replace: true,
+			link: function (scope, element){
+
+				/* Links redirects*/
+				var headerLinkToHome = function(){
+					scope.$emit('toShowLoadScreen');
+					state.go('home')
+				}
+
+				var headerLinkToGallery = function(){
+					scope.$emit('toShowLoadScreen');
+					state.go('gallery')
+				}
+
+				var headerLinkToAbout = function(){
+					scope.$emit('toShowLoadScreen');
+					state.go('about')
+				}
+
+				var headerLinkToBlog = function(){
+					scope.$emit('toShowLoadScreen');
+					state.go('blog')
+				}
+
+				var headerLinkToPortfolio = function(){
+					scope.$emit('toShowLoadScreen');
+					state.go('portfolio')
+				}
+
+				var headerLinkList = [
+					{
+						name: 'Home',
+						location: 'home',
+						iconClass: 'fa-home',
+						linkFunction: headerLinkToHome,
+						linkClass: 'link-home'
+					},
+					{
+						name: 'About',
+						location: 'about',
+						iconClass: 'fa-arrow-up',
+						linkFunction: headerLinkToAbout,
+						linkClass: 'link-about'
+					},
+					{
+						name: 'Gallery',
+						location: 'gallery',
+						iconClass: 'fa-arrow-right',
+						linkFunction: headerLinkToGallery,
+						linkClass: 'link-gallery'
+					},
+					{
+						name: 'Portfolio',
+						location: 'portfolios',
+						iconClass: 'fa-arrow-left',
+						linkFunction: headerLinkToPortfolio,
+						linkClass: 'link-portfolio'
+					},
+					{
+						name: 'Blog',
+						location: 'blog',
+						iconClass: 'fa-arrow-down',
+						linkFunction: headerLinkToBlog,
+						linkClass: 'link-blog'
+					},
+					
+				];
+
+				// Set all links to show (remove the link of current page)
+				scope.headerLinkToShow = headerLinkList.filter( function (link ){
+					return link.location != state.current.name;
+				});
+
+
+				/* Header behavior with the footer. Header hides, when footers shows and vice-versa */
+				/* Event to detect the apperance of footer */
+				scope.$on("footerIsRising", function(){
+					element[0].classList.add('hide-header');
+					element[0].classList.remove('show-header');
+				});
+				scope.$on("footerIsHiding", function(){
+					element[0].classList.add('show-header');
+					element[0].classList.remove('hide-header');
 				});
 			}
 		}
@@ -56695,36 +56695,6 @@ angular.module("common.pagination").directive("pagination",
 		}
 	}
 ]);
-angular.module("common.postList", []);
-
-angular.module("common.postList").factory("postListResource", [
-	'$resource',
-	'env',
-	function(
-		resource,
-		env
-	){
-		return resource(env.config.wordPressAPIURL + env.config.postList,
-		{},
-		{
-			list: {
-				method: 'GET',
-				isArray: true,
-				transformResponse: function(data, headersGetter, status){
-					
-					data = JSON.parse(data);
-
-					data.map( function(row){
-						row.header = headersGetter;
-						return row;
-					});
-
-		            return data;
-		        }
-			}
-		});
-	}
-]);
 angular.module("common.shareButtons", []);
 
 // Load any SKD in the project
@@ -56790,6 +56760,36 @@ angular.module("common.shareButtons").factory("loadFacebookSDK", function(){
 		   	}(document, 'script', 'facebook-jssdk'));
 	}
 })
+angular.module("common.postList", []);
+
+angular.module("common.postList").factory("postListResource", [
+	'$resource',
+	'env',
+	function(
+		resource,
+		env
+	){
+		return resource(env.config.wordPressAPIURL + env.config.postList,
+		{},
+		{
+			list: {
+				method: 'GET',
+				isArray: true,
+				transformResponse: function(data, headersGetter, status){
+					
+					data = JSON.parse(data);
+
+					data.map( function(row){
+						row.header = headersGetter;
+						return row;
+					});
+
+		            return data;
+		        }
+			}
+		});
+	}
+]);
 angular.module("site.about", []);
 angular.module('site.about').controller('aboutCtrl', [
 	'$scope',
