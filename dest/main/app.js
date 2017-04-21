de47113b1ddade1db540471c878436b0ac86041f
Amós Batista <amos.silva@gmail.com>
2017-04-21 16:19:01 -0300

    Twitter share link

--- dest/main/app.js
+++ dest/main/app.js
@@ -55873,106 +55873,6 @@ angular.module('common.easyScroll').directive('easyScrollContent', function(){
 		}
 	}
 });
-angular.module('common.header', []);
-
-angular.module('common.header').directive("myHeader", [
-	'$timeout',
-	'$state',
-	function(
-		timeout,
-		state
-	){
-		return {
-			restrict: "A",
-			templateUrl: "_header.html",
-			replace: true,
-			link: function (scope, element){
-
-				/* Links redirects*/
-				var headerLinkToHome = function(){
-					scope.$emit('toShowLoadScreen');
-					state.go('home')
-				}
-
-				var headerLinkToGallery = function(){
-					scope.$emit('toShowLoadScreen');
-					state.go('gallery')
-				}
-
-				var headerLinkToAbout = function(){
-					scope.$emit('toShowLoadScreen');
-					state.go('about')
-				}
-
-				var headerLinkToBlog = function(){
-					scope.$emit('toShowLoadScreen');
-					state.go('blog')
-				}
-
-				var headerLinkToPortfolio = function(){
-					scope.$emit('toShowLoadScreen');
-					state.go('portfolio')
-				}
-
-				var headerLinkList = [
-					{
-						name: 'Home',
-						location: 'home',
-						iconClass: 'fa-home',
-						linkFunction: headerLinkToHome,
-						linkClass: 'link-home'
-					},
-					{
-						name: 'About',
-						location: 'about',
-						iconClass: 'fa-arrow-up',
-						linkFunction: headerLinkToAbout,
-						linkClass: 'link-about'
-					},
-					{
-						name: 'Gallery',
-						location: 'gallery',
-						iconClass: 'fa-arrow-right',
-						linkFunction: headerLinkToGallery,
-						linkClass: 'link-gallery'
-					},
-					{
-						name: 'Portfolio',
-						location: 'portfolio',
-						iconClass: 'fa-arrow-left',
-						linkFunction: headerLinkToPortfolio,
-						linkClass: 'link-portfolio'
-					},
-					{
-						name: 'Blog',
-						location: 'blog',
-						iconClass: 'fa-arrow-down',
-						linkFunction: headerLinkToBlog,
-						linkClass: 'link-blog'
-					},
-					
-				];
-
-				// Set all links to show (remove the link of current page)
-				scope.headerLinkToShow = headerLinkList.filter( function (link ){
-					return link.location != state.current.name;
-				});
-
-
-				/* Header behavior with the footer. Header hides, when footers shows and vice-versa */
-				/* Event to detect the apperance of footer */
-				scope.$on("footerIsRising", function(){
-					element[0].classList.add('hide-header');
-					element[0].classList.remove('show-header');
-				});
-				scope.$on("footerIsHiding", function(){
-					element[0].classList.add('show-header');
-					element[0].classList.remove('hide-header');
-				});
-			}
-		}
-	}
-]);
 /* The footer directive*/
 angular.module('common.footer', []);
 
@@ -56079,6 +55979,106 @@ angular.module('common.footer').directive("myFooter", [
 						element[0].classList.add('hide-footer');
 						element[0].classList.remove('show-footer');
 					}
+				});
+			}
+		}
+	}
+]);
+angular.module('common.header', []);
+
+angular.module('common.header').directive("myHeader", [
+	'$timeout',
+	'$state',
+	function(
+		timeout,
+		state
+	){
+		return {
+			restrict: "A",
+			templateUrl: "_header.html",
+			replace: true,
+			link: function (scope, element){
+
+				/* Links redirects*/
+				var headerLinkToHome = function(){
+					scope.$emit('toShowLoadScreen');
+					state.go('home')
+				}
+
+				var headerLinkToGallery = function(){
+					scope.$emit('toShowLoadScreen');
+					state.go('gallery')
+				}
+
+				var headerLinkToAbout = function(){
+					scope.$emit('toShowLoadScreen');
+					state.go('about')
+				}
+
+				var headerLinkToBlog = function(){
+					scope.$emit('toShowLoadScreen');
+					state.go('blog')
+				}
+
+				var headerLinkToPortfolio = function(){
+					scope.$emit('toShowLoadScreen');
+					state.go('portfolio')
+				}
+
+				var headerLinkList = [
+					{
+						name: 'Home',
+						location: 'home',
+						iconClass: 'fa-home',
+						linkFunction: headerLinkToHome,
+						linkClass: 'link-home'
+					},
+					{
+						name: 'About',
+						location: 'about',
+						iconClass: 'fa-arrow-up',
+						linkFunction: headerLinkToAbout,
+						linkClass: 'link-about'
+					},
+					{
+						name: 'Gallery',
+						location: 'gallery',
+						iconClass: 'fa-arrow-right',
+						linkFunction: headerLinkToGallery,
+						linkClass: 'link-gallery'
+					},
+					{
+						name: 'Portfolio',
+						location: 'portfolio',
+						iconClass: 'fa-arrow-left',
+						linkFunction: headerLinkToPortfolio,
+						linkClass: 'link-portfolio'
+					},
+					{
+						name: 'Blog',
+						location: 'blog',
+						iconClass: 'fa-arrow-down',
+						linkFunction: headerLinkToBlog,
+						linkClass: 'link-blog'
+					},
+					
+				];
+
+				// Set all links to show (remove the link of current page)
+				scope.headerLinkToShow = headerLinkList.filter( function (link ){
+					return link.location != state.current.name;
+				});
+
+
+				/* Header behavior with the footer. Header hides, when footers shows and vice-versa */
+				/* Event to detect the apperance of footer */
+				scope.$on("footerIsRising", function(){
+					element[0].classList.add('hide-header');
+					element[0].classList.remove('show-header');
+				});
+				scope.$on("footerIsHiding", function(){
+					element[0].classList.add('show-header');
+					element[0].classList.remove('hide-header');
 				});
 			}
 		}
@@ -56490,38 +56490,14 @@ angular.module("common.shareButtons").directive("shareButtons", [
 			},
 			link: function (scope, element){
 
-				// Linkedin
-				scope.linkedinShare = function(){
+				// Twitter
+				scope.twitterShareLink = 'https://twitter.com/intent/tweet?'
+					+ 'text=' + encodeURI(scope.shareOptions.title) + ': ' + encodeURI(scope.shareOptions.description) + "&"
+					+ 'url=' + encodeURI(window.location.href) + "&"
+					+ 'via=amosbatista&'
+					+ 'related=amosbatista'
 
-					var payload = { 
-						content: {
-							title: scope.shareOptions.title,
-							description: scope.shareOptions.description,
-							"submitted-url": window.location.href,
-							"submitted-image-url": scope.shareOptions.imageUrl || window.location.origin + "/img/" + scope.shareOptions.imageName
-
-						},
-						visibility: { 
-							code: "anyone"
-						} 
-					};
-
-					/*var header = {
-						Authorization: 'Bearer ' + linkedinAPI.ENV.auth.anonymous_token
-					}*/
-
-					/*linkedinAPI.API.Raw("/people/~/shares?format=json")
-						.method("POST")
-						.header(JSON.stringify(header))
-						.body(JSON.stringify(payload))
-						.result(function(data){
-							console.log("Success", data);
-						})
-						.error(function(data){
-							console.log("Error", data);
-						});*/
-
-				}
+				
 
 				// Linkedin Direct Link
 				scope.linkedinShareLink = 'https://www.linkedin.com/shareArticle?'
--- src/common/shareButtons/_shareButtonDIR.js
+++ src/common/shareButtons/_shareButtonDIR.js
@@ -13,38 +13,14 @@ angular.module("common.shareButtons").directive("shareButtons", [
 			},
 			link: function (scope, element){
 
-				// Linkedin
-				scope.linkedinShare = function(){
+				// Twitter
+				scope.twitterShareLink = 'https://twitter.com/intent/tweet?'
+					+ 'text=' + encodeURI(scope.shareOptions.title) + ': ' + encodeURI(scope.shareOptions.description) + "&"
+					+ 'url=' + encodeURI(window.location.href) + "&"
+					+ 'via=amosbatista&'
+					+ 'related=amosbatista'
 
-					var payload = { 
-						content: {
-							title: scope.shareOptions.title,
-							description: scope.shareOptions.description,
-							"submitted-url": window.location.href,
-							"submitted-image-url": scope.shareOptions.imageUrl || window.location.origin + "/img/" + scope.shareOptions.imageName
-
-						},
-						visibility: { 
-							code: "anyone"
-						} 
-					};
-
-					/*var header = {
-						Authorization: 'Bearer ' + linkedinAPI.ENV.auth.anonymous_token
-					}*/
-
-					/*linkedinAPI.API.Raw("/people/~/shares?format=json")
-						.method("POST")
-						.header(JSON.stringify(header))
-						.body(JSON.stringify(payload))
-						.result(function(data){
-							console.log("Success", data);
-						})
-						.error(function(data){
-							console.log("Error", data);
-						});*/
-
-				}
+				
 
 				// Linkedin Direct Link
 				scope.linkedinShareLink = 'https://www.linkedin.com/shareArticle?'
